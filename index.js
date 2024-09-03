6b4fb698d08152bef1e401c0f546e8eef9ae06c8
trafdual <traz08102003@gmail.com>
2024-09-03 17:46:50 +0700

    f

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use('/', apinewsanpham)
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.listen(3003, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000')
   console.log(__dirname)
 })
