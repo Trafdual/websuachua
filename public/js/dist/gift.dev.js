592f91816ae8026a6a1c48e25fd1a3e1c4adf64f
trafdual <traz08102003@gmail.com>
2024-09-12 20:11:09 +0700

    f

--- public/js/dist/gift.dev.js
+++ public/js/dist/gift.dev.js
@@ -1,114 +1,162 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+;
+
 (function () {
   var $ = document.querySelector.bind(document);
   var timeRotate = 7000;
   var currentRotate = 0;
   var isRotating = false;
   var wheel = $('.wheel');
-  var btnWheel = $('.btn--wheel');
-  var showMsg = $('.msg'); // Danh sách phần thưởng với hình ảnh
+  var btnWheel = $('#thuong'); // Nút Quay thưởng
 
+  var btnWheel1 = $('#thu'); // Nút Quay thử
+
+  var showMsg = $('.msg');
   var listGift = [{
-    text: 'Sạc dự phòng 10.000W',
-    percent: 10 / 100,
-    img: 'http://localhost:8080/images/review-item1.jpg'
-  }, {
     text: 'Wave Alpha 110',
-    percent: 10 / 100,
-    img: 'images/review-item2.jpg'
+    percent: 0.1
   }, {
     text: 'Quay thêm lượt',
-    percent: 5 / 100,
-    img: 'images/review-item1.jpg'
+    percent: 0.05
+  }, {
+    text: '150.000đ',
+    percent: 0.2
+  }, {
+    text: 'Sạc dự phòng 10.000W',
+    percent: 0.1
   }, {
     text: 'Tai nghe không dây',
-    percent: 5 / 100,
-    img: 'images/review-item1.jpg'
+    percent: 0.05
   }, {
-    text: 'Iphone 13',
-    percent: 40 / 100,
-    img: 'images/review-item1.jpg'
+    text: 'Iphone 13 New',
+    percent: 0.2
   }, {
     text: '5.000.000đ',
-    percent: 20 / 100,
-    img: 'images/review-item2.jpg'
-  }]; // Số lượng phần thưởng
-
-  var size = listGift.length; // Số đo góc của 1 phần thưởng chiếm trên hình tròn
-
-  var rotate = 360 / size; // Số đo góc cần để tạo độ nghiêng, 90 độ trừ đi góc của 1 phần thưởng chiếm
-
+    percent: 0.1
+  }, {
+    text: 'Thay pin miễn phí không giới hạn',
+    percent: 0.2
+  }];
+  var size = listGift.length;
+  var rotate = 360 / size;
   var skewY = 90 - rotate;
   listGift.forEach(function (item, index) {
-    // Tạo thẻ li
-    var elm = document.createElement('li'); // Xoay và tạo độ nghiêng cho các thẻ li
-
-    elm.style.transform = "rotate(".concat(rotate * index, "deg) skewY(-").concat(skewY, "deg)"); // Thêm văn bản và hình ảnh vào thẻ li
-
+    var elm = document.createElement('li');
+    elm.style.transform = "rotate(".concat(rotate * index, "deg) skewY(-").concat(skewY, "deg)");
     var textClasses = ['text-1', 'text-2'];
     var textClass = textClasses[index % textClasses.length];
-    elm.innerHTML = "\n    <p style=\"transform: skewY(".concat(skewY, "deg) rotate(").concat(rotate / 2, "deg);\" class=\"text ").concat(textClass, "\">\n        <b>").concat(item.text, "</b>\n    </p>\n    <img src=\"").concat(item.img, "\" alt=\"").concat(item.text, "\" class=\"image\">\n"); // Thêm vào thẻ ul
-
+    elm.innerHTML = "\n            <p style=\"transform: skewY(".concat(skewY, "deg) rotate(").concat(rotate / 2, "deg);\" class=\"text ").concat(textClass, "\">\n                <b>").concat(item.text, "</b>\n            </p>\n        ");
     wheel.appendChild(elm);
-  }); // Hàm bắt đầu
+  });
 
-  var start = function start() {
+  var start = function start(isTest) {
     showMsg.innerHTML = '';
-    isRotating = true; // Lấy 1 số ngẫu nhiên 0 -> 1
-
-    var random = Math.random(); // Gọi hàm lấy phần thưởng
-
-    var gift = getGift(random); // Số vòng quay: 360 độ = 1 vòng (Góc quay hiện tại)
-
-    currentRotate += 360 * 10; // Gọi hàm quay
-
-    rotateWheel(currentRotate, gift.index); // Gọi hàm in ra màn hình
-
+    isRotating = true;
+    var random = Math.random();
+    var gift = getGift(random, isTest);
+    currentRotate += 360 * 10;
+    rotateWheel(currentRotate, gift.index);
     showGift(gift);
-  }; // Hàm quay vòng quay
-
+  };
 
   var rotateWheel = function rotateWheel(currentRotate, index) {
-    $('.wheel').style.transform = "rotate(".concat( // Góc quay hiện tại trừ góc của phần thưởng
-    // Trừ tiếp cho một nửa góc của 1 phần thưởng để đưa mũi tên về chính giữa
-    currentRotate - index * rotate - rotate / 2, "deg)");
-  }; // Hàm lấy phần thưởng, chỉ chọn hai phần thưởng cụ thể
+    wheel.style.transform = "rotate(".concat(currentRotate - index * rotate - rotate / 2, "deg)");
+  };
 
+  var getGift = function getGift(randomNumber, isTest) {
+    if (isTest) {
+      var _ret = function () {
+        var prizes = [{
+          text: 'Wave Alpha 110',
+          percent: 0.1
+        }, {
+          text: 'Iphone 13 New',
+          percent: 0.2
+        }, {
+          text: 'Other',
+          percent: 0.7
+        }];
+        var cumulativePercent = 0;
 
-  var getGift = function getGift(randomNumber) {
-    // Danh sách phần thưởng chỉ quay vào hai phần thưởng cụ thể
-    var specialGifts = [{
-      text: 'Tai nghe không dây',
-      index: listGift.findIndex(function (gift) {
+        var _loop = function _loop(i) {
+          cumulativePercent += prizes[i].percent;
+
+          if (randomNumber < cumulativePercent) {
+            if (prizes[i].text === 'Other') {
+              var otherGifts = listGift.filter(function (gift) {
+                return gift.text !== 'Wave Alpha 110' && gift.text !== 'Iphone 13 New';
+              });
+              var randomOtherIndex = Math.floor(Math.random() * otherGifts.length);
+              return {
+                v: {
+                  v: _objectSpread({}, otherGifts[randomOtherIndex], {
+                    index: listGift.indexOf(otherGifts[randomOtherIndex])
+                  })
+                }
+              };
+            } else {
+              var selectedIndex = listGift.findIndex(function (gift) {
+                return gift.text === prizes[i].text;
+              });
+              return {
+                v: {
+                  v: _objectSpread({}, listGift[selectedIndex], {
+                    index: selectedIndex
+                  })
+                }
+              };
+            }
+          }
+        };
+
+        for (var i = 0; i < prizes.length; i++) {
+          var _ret2 = _loop(i);
+
+          if (_typeof(_ret2) === "object") return _ret2.v;
+        }
+      }();
+
+      if (_typeof(_ret) === "object") return _ret.v;
+    } else {
+      var fixedGiftIndex = listGift.findIndex(function (gift) {
         return gift.text === 'Tai nghe không dây';
-      })
-    }, {
-      text: 'Tai nghe không dây',
-      index: listGift.findIndex(function (gift) {
-        return gift.text === 'Tai nghe không dây';
-      })
-    }]; // Xác suất cho các phần thưởng đặc biệt
+      });
+      return _objectSpread({}, listGift[fixedGiftIndex], {
+        index: fixedGiftIndex
+      });
+    }
+  };
 
-    var specialGiftsPercent = 0.5; // 50% cho mỗi phần thưởng đặc biệt
-    // Chọn phần thưởng dựa trên số ngẫu nhiên
-
-    var randomSpecial = Math.random();
-    var selectedGift = specialGifts[Math.floor(randomSpecial * 2)];
-    return selectedGift;
-  }; // In phần thưởng ra màn hình
-
+  var updateUserInfo = function updateUserInfo(gift) {
+    var userInfoList = document.querySelector('.user-info');
+    var listItem = document.createElement('p');
+    listItem.classList.add('thongtin');
+    listItem.textContent = "B\u1EA1n \u0111\xE3 tr\xFAng \"".concat(gift.text, "\"");
+    userInfoList.appendChild(listItem); // Thêm thẻ <p> mới vào danh sách user-info
+  };
 
   var showGift = function showGift(gift) {
     var timer = setTimeout(function () {
       isRotating = false;
-      showMsg.innerHTML = "Ch\xFAc m\u1EEBng b\u1EA1n \u0111\xE3 nh\u1EADn \u0111\u01B0\u1EE3c \"".concat(listGift[gift.index].text, "\"");
+      showMsg.innerHTML = "Ch\xFAc m\u1EEBng b\u1EA1n \u0111\xE3 nh\u1EADn \u0111\u01B0\u1EE3c \"".concat(gift.text, "\"");
+      updateUserInfo(gift);
       clearTimeout(timer);
     }, timeRotate);
-  }; // Sự kiện click button start
-
+  };
 
   btnWheel.addEventListener('click', function () {
-    !isRotating && start();
+    !isRotating && start(false);
+  });
+  btnWheel1.addEventListener('click', function () {
+    !isRotating && start(true);
   });
 })();
--- public/js/gift.js
+++ public/js/gift.js
@@ -1,112 +1,124 @@
-(() => {
-    const $ = document.querySelector.bind(document);
+;(() => {
+  const $ = document.querySelector.bind(document)
 
-    let timeRotate = 7000; 
-    let currentRotate = 0;
-    let isRotating = false;
-    const wheel = $('.wheel');
-    const btnWheel = $('#thuong'); // Nút Quay thưởng
-    const btnWheel1 = $('#thu');   // Nút Quay thử
+  let timeRotate = 7000
+  let currentRotate = 0
+  let isRotating = false
+  const wheel = $('.wheel')
+  const btnWheel = $('#thuong') // Nút Quay thưởng
+  const btnWheel1 = $('#thu') // Nút Quay thử
 
-    const showMsg = $('.msg');
+  const showMsg = $('.msg')
 
-    const listGift = [
-        { text: 'Wave Alpha 110', percent: 0.1 },
-        { text: 'Quay thêm lượt', percent: 0.05 },
-        { text: '150.000đ', percent: 0.2 },
-        { text: 'Sạc dự phòng 10.000W', percent: 0.1 },
-        { text: 'Tai nghe không dây', percent: 0.05 },
-        { text: 'Iphone 13 New', percent: 0.4 },
-        { text: '5.000.000đ', percent: 0.2 },
-        { text: 'Thay pin miễn phí không giới hạn', percent: 0.2 }
-    ];
+  const listGift = [
+    { text: 'Wave Alpha 110', percent: 0.1 },
+    { text: 'Quay thêm lượt', percent: 0.05 },
+    { text: '150.000đ', percent: 0.2 },
+    { text: 'Sạc dự phòng 10.000W', percent: 0.1 },
+    { text: 'Tai nghe không dây', percent: 0.05 },
+    { text: 'Iphone 13 New', percent: 0.2 },
+    { text: '5.000.000đ', percent: 0.1 },
+    { text: 'Thay pin miễn phí không giới hạn', percent: 0.2 }
+  ]
 
-    const size = listGift.length;
-    const rotate = 360 / size;
-    const skewY = 90 - rotate;
+  const size = listGift.length
+  const rotate = 360 / size
+  const skewY = 90 - rotate
 
-    listGift.forEach((item, index) => {
-        const elm = document.createElement('li');
-        elm.style.transform = `rotate(${rotate * index}deg) skewY(-${skewY}deg)`;
-        const textClasses = ['text-1', 'text-2'];
-        const textClass = textClasses[index % textClasses.length];
-        elm.innerHTML = `
-            <p style="transform: skewY(${skewY}deg) rotate(${rotate / 2}deg);" class="text ${textClass}">
+  listGift.forEach((item, index) => {
+    const elm = document.createElement('li')
+    elm.style.transform = `rotate(${rotate * index}deg) skewY(-${skewY}deg)`
+    const textClasses = ['text-1', 'text-2']
+    const textClass = textClasses[index % textClasses.length]
+    elm.innerHTML = `
+            <p style="transform: skewY(${skewY}deg) rotate(${
+      rotate / 2
+    }deg);" class="text ${textClass}">
                 <b>${item.text}</b>
             </p>
-        `;
-        wheel.appendChild(elm);
-    });
+        `
+    wheel.appendChild(elm)
+  })
 
-    const start = (isTest) => {
-        showMsg.innerHTML = '';
-        isRotating = true;
+  const start = isTest => {
+    showMsg.innerHTML = ''
+    isRotating = true
 
-        const random = Math.random();
-        const gift = getGift(random, isTest);
+    const random = Math.random()
+    const gift = getGift(random, isTest)
 
-        currentRotate += 360 * 10;
-        rotateWheel(currentRotate, gift.index);
-        showGift(gift);
-    };
+    currentRotate += 360 * 10
+    rotateWheel(currentRotate, gift.index)
+    showGift(gift)
+  }
 
-    const rotateWheel = (currentRotate, index) => {
-        wheel.style.transform = `rotate(${
-            currentRotate - index * rotate - rotate / 2
-        }deg)`;
-    };
+  const rotateWheel = (currentRotate, index) => {
+    wheel.style.transform = `rotate(${
+      currentRotate - index * rotate - rotate / 2
+    }deg)`
+  }
 
-    const getGift = (randomNumber, isTest) => {
-        if (isTest) {
-            const prizes = [
-                { text: 'Wave Alpha 110', percent: 0.1 },
-                { text: 'Iphone 13', percent: 0.2 },
-                { text: 'Other', percent: 0.7 }
-            ];
-            let cumulativePercent = 0;
-            for (let i = 0; i < prizes.length; i++) {
-                cumulativePercent += prizes[i].percent;
-                if (randomNumber < cumulativePercent) {
-                    if (prizes[i].text === 'Other') {
-                        const otherGifts = listGift.filter(
-                            gift => gift.text !== 'Wave Alpha 110' && gift.text !== 'Iphone 13'
-                        );
-                        const randomOtherIndex = Math.floor(Math.random() * otherGifts.length);
-                        return { ...otherGifts[randomOtherIndex], index: listGift.indexOf(otherGifts[randomOtherIndex]) };
-                    } else {
-                        const selectedIndex = listGift.findIndex(gift => gift.text === prizes[i].text);
-                        return { ...listGift[selectedIndex], index: selectedIndex };
-                    }
-                }
+  const getGift = (randomNumber, isTest) => {
+    if (isTest) {
+      const prizes = [
+        { text: 'Wave Alpha 110', percent: 0.1 },
+        { text: 'Iphone 13 New', percent: 0.2 },
+        { text: 'Other', percent: 0.7 }
+      ]
+      let cumulativePercent = 0
+      for (let i = 0; i < prizes.length; i++) {
+        cumulativePercent += prizes[i].percent
+        if (randomNumber < cumulativePercent) {
+          if (prizes[i].text === 'Other') {
+            const otherGifts = listGift.filter(
+              gift =>
+                gift.text !== 'Wave Alpha 110' && gift.text !== 'Iphone 13 New'
+            )
+            const randomOtherIndex = Math.floor(
+              Math.random() * otherGifts.length
+            )
+            return {
+              ...otherGifts[randomOtherIndex],
+              index: listGift.indexOf(otherGifts[randomOtherIndex])
             }
-        } else {
-            const fixedGiftIndex = listGift.findIndex(gift => gift.text === 'Tai nghe không dây');
-            return { ...listGift[fixedGiftIndex], index: fixedGiftIndex };
+          } else {
+            const selectedIndex = listGift.findIndex(
+              gift => gift.text === prizes[i].text
+            )
+            return { ...listGift[selectedIndex], index: selectedIndex }
+          }
         }
-    };
+      }
+    } else {
+      const fixedGiftIndex = listGift.findIndex(
+        gift => gift.text === 'Tai nghe không dây'
+      )
+      return { ...listGift[fixedGiftIndex], index: fixedGiftIndex }
+    }
+  }
 
-    const updateUserInfo = (gift) => {
-        const userInfoList = document.querySelector('.user-info');
-        const listItem = document.createElement('p'); 
-        listItem.classList.add('thongtin'); 
-        listItem.textContent = `Bạn đã trúng "${gift.text}"`; 
-        userInfoList.appendChild(listItem); // Thêm thẻ <p> mới vào danh sách user-info
-    };
+  const updateUserInfo = gift => {
+    const userInfoList = document.querySelector('.user-info')
+    const listItem = document.createElement('p')
+    listItem.classList.add('thongtin')
+    listItem.textContent = `Bạn đã trúng "${gift.text}"`
+    userInfoList.appendChild(listItem) // Thêm thẻ <p> mới vào danh sách user-info
+  }
 
-    const showGift = gift => {
-        let timer = setTimeout(() => {
-            isRotating = false;
-            showMsg.innerHTML = `Chúc mừng bạn đã nhận được "${gift.text}"`;
-            updateUserInfo(gift);
-            clearTimeout(timer);
-        }, timeRotate);
-    };
+  const showGift = gift => {
+    let timer = setTimeout(() => {
+      isRotating = false
+      showMsg.innerHTML = `Chúc mừng bạn đã nhận được "${gift.text}"`
+      updateUserInfo(gift)
+      clearTimeout(timer)
+    }, timeRotate)
+  }
 
-    btnWheel.addEventListener('click', () => {
-        !isRotating && start(false); 
-    });
+  btnWheel.addEventListener('click', () => {
+    !isRotating && start(false)
+  })
 
-    btnWheel1.addEventListener('click', () => {
-        !isRotating && start(true);
-    });
-})();
+  btnWheel1.addEventListener('click', () => {
+    !isRotating && start(true)
+  })
+})()
